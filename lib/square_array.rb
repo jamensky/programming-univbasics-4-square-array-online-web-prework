4ab43980c8dea953d60572061264f553b24fb0ff
Jamal Rjoul <jamensky1991@gmail.com>
2019-10-28 16:23:21 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,7 +4,7 @@ def square_array(array)
   i = 0
   new_array = []
   while i < array.length do
-   new_array.push(array[i] ** array[i])
+   new_array.push(array[i] * array[i])
     i += 1
   end
   return new_array
